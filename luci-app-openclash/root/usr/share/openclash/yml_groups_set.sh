91b5ed3d4c5f3a98f3f3b360c2598a2c67fd6725
kenzok8 <kenzok8@gmail.com>
2020-10-02 23:38:25 +0800

    fix JD签到

--- luci-app-openclash/root/usr/share/openclash/yml_groups_set.sh
+++ luci-app-openclash/root/usr/share/openclash/yml_groups_set.sh
@@ -173,7 +173,7 @@ yml_groups_set()
    fi
    
    #游戏策略组存在时判断节点是否存在
-   if [ ! -z "$if_game_group" ] && [ ! -z "$(grep "^ \{0,\}- name: $if_game_group" "$CONFIG_GROUP_FILE")" ]; then
+   if [ -n "$if_game_group" ] && [ -n "$(grep "^$if_game_group$" /tmp/Proxy_Group)" ]; then
       config_foreach yml_servers_add "servers" "$name" "$type" "check" #加入服务器节点
       config_foreach set_proxy_provider "proxy-provider" "$group_name" "check" #加入代理集
       return
@@ -199,6 +199,8 @@ yml_groups_set()
    
    if [ "$type" = "select" ] || [ "$type" = "relay" ]; then
       config_list_foreach "$section" "other_group" set_other_groups #加入其他策略组
+   else
+      config_list_foreach "$section" "other_group_dr" set_other_groups #仅加入direct/reject其他策略组
    fi
    
    config_foreach yml_servers_add "servers" "$name" "$type" #加入服务器节点
