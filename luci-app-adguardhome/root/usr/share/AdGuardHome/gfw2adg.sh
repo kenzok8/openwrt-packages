3b3064e4713081261bb84d1ce04fe73493025e3c
kenzok8 <kenzok8@gmail.com>
2021-03-04 10:58:30 +0000

    同步Lienol

--- luci-app-adguardhome/root/usr/share/AdGuardHome/gfw2adg.sh
+++ luci-app-adguardhome/root/usr/share/AdGuardHome/gfw2adg.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
 PATH="/usr/sbin:/usr/bin:/sbin:/bin"
-
 checkmd5(){
 local nowmd5=$(md5sum /tmp/adguard.list 2>/dev/null)
 nowmd5=${nowmd5%% *}
@@ -12,7 +10,6 @@ if [ "$nowmd5" != "$lastmd5" ]; then
 	[ "$1" == "noreload" ] || /etc/init.d/AdGuardHome reload
 fi
 }
-
 configpath=$(uci get AdGuardHome.AdGuardHome.configpath 2>/dev/null)
 [ "$1" == "del" ] && sed -i '/programaddstart/,/programaddend/d' $configpath && checkmd5 "$2" && exit 0
 gfwupstream=$(uci get AdGuardHome.AdGuardHome.gfwupstream 2>/dev/null)
@@ -23,7 +20,7 @@ if [ ! -f "$configpath" ]; then
 	echo "please make a config first"
 	exit 1
 fi
-wget-ssl --no-check-certificate https://cdn.jsdelivr.net/gh/gfwlist/gfwlist/gfwlist.txt -O- | base64 -d > /tmp/gfwlist.txt
+wget --no-check-certificate https://cdn.jsdelivr.net/gh/gfwlist/gfwlist/gfwlist.txt -O- | base64 -d > /tmp/gfwlist.txt
 cat /tmp/gfwlist.txt | awk -v upst="$gfwupstream" 'BEGIN{getline;}{
 s1=substr($0,1,1);
 if (s1=="!")
