f3ca53cb4730c53b8dc98f5043fe9ce51425cfd9
kenzok8 <kenzok8@gmail.com>
2021-04-20 23:59:16 +0800

    Update serverchan.lua

--- luci-app-serverchan/luasrc/controller/serverchan.lua
+++ luci-app-serverchan/luasrc/controller/serverchan.lua
@@ -18,7 +18,7 @@ end
 
 function act_status()
 	local e={}
-	e.running=luci.sys.call("ps|grep -v grep|grep -c serverchan >/dev/null")==0
+	e.running=luci.sys.call("busybox ps|grep -v grep|grep -c serverchan >/dev/null")==0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
