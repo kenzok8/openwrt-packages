e08aabbe06e2f8583d4ef7e15fdc187124960f7f
kenzok8 <kenzok8@gmail.com>
2021-06-10 17:01:11 +0800

    避免多次添加冗余地址

--- luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+++ luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
@@ -327,7 +327,7 @@ local function processData(szType, content)
 			if not params.type or params.type == "tcp" then
 				if params.security == "xtls" then
 					result.xtls = "1"
-					result.tls_host = params.sni or host[1]
+					result.tls_host = params.sni
 					result.vless_flow = params.flow
 				else
 					result.xtls = "0"
@@ -338,7 +338,7 @@ local function processData(szType, content)
 				result.ws_path = params.path or "/"
 			end
 			if params.type == 'http' then
-				result.h2_host = params.host or host[1]
+				result.h2_host = params.host
 				result.h2_path = params.path or "/"
 			end
 			if params.type == 'kcp' then
@@ -362,7 +362,7 @@ local function processData(szType, content)
 			
 			if params.security == "tls" then
 				result.tls = "1"
-				result.tls_host = params.sni or host[1]
+				result.tls_host = params.sni
 			else
 				result.tls = "0"
 			end
@@ -462,7 +462,7 @@ local execute = function()
 						-- log(result)
 						if result then
 							-- 中文做地址的 也没有人拿中文域名搞，就算中文域也有Puny Code SB 机场
-							if not result.server or not result.server_port or result.alias == "NULL" or check_filer(result) or result.server:match("[^0-9a-zA-Z%-%.%s]") then
+							if not result.server or not result.server_port or result.alias == "NULL" or check_filer(result) or result.server:match("[^0-9a-zA-Z%-%.%s]") or cache[groupHash][result.hashkey] then
 								log('丢弃无效节点: ' .. result.type .. ' 节点, ' .. result.alias)
 							else
 								-- log('成功解析: ' .. result.type ..' 节点, ' .. result.alias)
