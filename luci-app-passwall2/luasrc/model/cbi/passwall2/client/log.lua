08d2bf96ca3ca7ad7bdaef00c18019ce9670101c
kenzok8 <kenzok8@gmail.com>
2023-04-27 03:31:14 +0000

    update

--- luci-app-passwall2/luasrc/model/cbi/passwall2/client/log.lua
+++
@@ -1,8 +0,0 @@
-local api = require "luci.passwall2.api"
-local appname = api.appname
-
-f = SimpleForm(appname)
-f.reset = false
-f.submit = false
-f:append(Template(appname .. "/log/log"))
-return f
--- luci-app-ssr-plus/root/usr/share/shadowsocksr/chinaipset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-[ -f "$1" ] && china_ip=$1
-ipset -! flush china 2>/dev/null
-ipset -! -R <<-EOF || exit 1
-	create china hash:net
-	$(cat ${china_ip:=/etc/ssrplus/china_ssr.txt} | sed -e "s/^/add china /")
-EOF
