def93167e228b34cc0bb8eb2cca585012d697310
actions-user <kenzok8@users.noreply.github.com>
2026-01-29 14:31:38 +0800

    update 2026-01-29 14:31:38

--- luci-app-dockerman/luasrc/model/cbi/dockerman/containers.lua
+++ luci-app-dockerman/luasrc/model/cbi/dockerman/containers.lua
@@ -85,7 +85,7 @@ function get_containers()
 		end
 		for ii,iv in ipairs(images) do
 			if iv.Id == v.ImageID then
-				data[index]["_image"] = iv.RepoTags and iv.RepoTags[1] or (next(iv.RepoDigests) and (iv.RepoDigests[1]:gsub("(.-)@.+", "%1") .. ":&lt;none&gt;")) or ""
+				data[index]["_image"] = (iv.RepoTags and iv.RepoTags[1]) or (iv.RepoDigests and next(iv.RepoDigests) and (iv.RepoDigests[1]:gsub("(.-)@.+", "%1") .. ":&lt;none&gt;")) or "&lt;none&gt;:&lt;none&gt;"
 			end
 		end
 		data[index]["_id_name"] = '<a href='..luci.dispatcher.build_url("admin/docker/container/"..v.Id)..'  class="dockerman_link" title="'..translate("Container detail")..'">'.. data[index]["_name"] .. "<br><font color='#9f9f9f'>ID: " ..	data[index]["_id"]
